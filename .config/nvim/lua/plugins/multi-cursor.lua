746a37838f28145be1205ed1fe72c7d59fc2ab39
Redhwan Nacef <nacefredhwan@gmail.com>
2025-12-12 10:47:10 +0000

    Add vertical cursors with alt instead of control

--- .config/nvim/lua/plugins/multi-cursor.lua
+++ .config/nvim/lua/plugins/multi-cursor.lua
@@ -2,5 +2,10 @@ return {
   {
     "mg979/vim-visual-multi",
     branch = "master",
+    config = function()
+      local opts = { silent = true, noremap = false }
+      vim.keymap.set({ "n", "v" }, "<A-Down>", "<Plug>(VM-Add-Cursor-Down)", opts)
+      vim.keymap.set({ "n", "v" }, "<A-Up>", "<Plug>(VM-Add-Cursor-Up)", opts)
+    end,
   },
 }
